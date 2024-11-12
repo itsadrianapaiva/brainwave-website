51f9796d70e3fce54e9979ffea62ac8abcc670fc
unknown <adrianapaivausa@icloud.com>
2024-11-12 16:15:30 -0500

    button component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import ButtonGradient from "./assets/svg/ButtonGradient.tsx";
+import Button from "./components/Button.tsx";
+import Header from "./components/Header.tsx";
 
-function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Router>
-    <App />
+      <App />
     </Router>
-  </StrictMode>,
-)
+  </StrictMode>
+);
