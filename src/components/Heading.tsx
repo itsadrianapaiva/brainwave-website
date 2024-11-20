2d61a85ecdace6400fc21efd3f2fb566b913c172
unknown <adrianapaivausa@icloud.com>
2024-11-19 19:27:08 -0500

    heading fixed

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -2,7 +2,7 @@
 
 const Heading: React.FC<{ className?: string; title?: string}> = ({ className, title }) => {
   return (
-    <div className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20`}>
+    <div className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20 md:text-center`}>
       {title && <h2 className="h2">{title}</h2>}
     </div>
   )
