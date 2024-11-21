32ffae7c507a33ea19b24a37a2ab37fea1dedf0d
unknown <adrianapaivausa@icloud.com>
2024-11-21 17:36:08 -0500

    starting service card 2

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,4 +1,4 @@
-import { service1, check } from "../assets";
+import { service1, check, service2 } from "../assets";
 import { brainwaveServices } from "../constants";
 import Generating from "./Generating";
 import Heading from "./Heading";
@@ -51,8 +51,23 @@ const Services: React.FC = () => {
           </div>
 
           <div className="relative z-1 grid gap-5 lg:grid-cols-2">
-            <div>
+            <div className="relative min-h-[39rem] border border-n-1/10 rounded-3xl overflow-hidden">
+              <div className="absolute inset-0">
+                <img
+                  src={service2}
+                  className="h-full w-full object-cover"
+                  width={630}
+                  height={750}
+                  alt="robot"
+                />
+              </div>
 
+              <div className="absolute inset-0 flex flex-col justify-end p-8 bg-gradient-to-b from-n-8/0 to-n-8/90 lg:p-15">
+                <h4 className="h4 mb-4">Photo editing</h4>
+                <p className="body-2 mb-[3rem] text-n-3">
+                  Automatically enhance your photos using our AI app's photo editing feature. Try it now!
+                </p>
+              </div>
             </div>
           </div>
         </div>
